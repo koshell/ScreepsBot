0a2ed104a37b721d56c194cfe8062a35e77b461e
zone1135 <zone1135@gmail.com>
2019-12-08 16:10:52 +1100

    SyntaxError: Unexpected token .     at Object.<anonymous>:2:150615     at Object.r.run:2:151124

--- bot/main.js
+++ bot/main.js
@@ -1,1 +1,72 @@
 var MunkresJS = require("munkres");
+
+// Main Loop
+module.exports.loop = function () {
+    for (const i in Game.creeps) {
+        if (Game.creeps[i].memory.mining == true){
+            energyMiner(Game.creeps[i]);
+        } else {
+            switch(Game.creeps[i].memory.job){
+                case 'warrior':
+                    break;
+                case 'unemployed':
+                    Game.creeps[i].memory.goingToMine = true;
+                    Game.creeps[i].memory.mining = true;
+                    energyMiner(Game.creeps[i]);
+                    break;
+                case 'text':
+                    break;
+                default:
+                    console.log(`Creep ${Game.creeps[i].name} has invalid job ${Game.creeps[i].memory.job}.`);
+            }
+        }
+    }
+}
+
+function checkGameState(){
+    console.log("Check game state is incomplete");
+}
+
+// START OF JOBS
+
+function energyMiner(creep){
+    if (creep.memory.goingToMine) {
+        const target = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+        if (target) {
+            if(creep.harvest(target) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target);
+            }
+        }
+        if (creep.store.getFreeCapacity(RESOURCE_ENERGY) < 1) {
+            creep.memory.goingToMine = false;
+        }
+    } else {
+        if (creep.store.getUsedCapacity(RESOURCE_ENERGY) != 0){
+
+            const energyStructure = creep.room.find(FIND_HOSTILE_CREEPS, {
+                filter: function(object) {
+                    return object.store.getFreeCapacity(RESOURCE_ENERGY) => creep.store.getCapacity(RESOURCE_ENERGY);
+                }
+            });
+
+            if (energyStructure) {
+                if(creep.transfer(energyStructure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(energyStructure);
+                }
+            }
+
+        } else {
+            creep.memory.mining = false;
+        }
+    }
+}
+
+function warrior(creep){
+    console.log("Warrior is incomplete.");
+}
+
+function engineer(creep){
+    console.log("Engineer is incomplete.");
+}
+
+// END OF JOBS
